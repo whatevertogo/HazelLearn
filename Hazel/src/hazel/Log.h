18c6b83aba17ce98fd5646ec7bafe1a4e199f4f7
whatevertogo <149563971+whatevertogo@users.noreply.github.com>
2025-02-10 01:17:42 +0800

    7

--- Hazel/src/hazel/Log.h
+++ Hazel/src/hazel/Log.h
@@ -2,7 +2,7 @@
 
 #include "Core.h"
 #include "spdlog/spdlog.h"
-#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/sinks/stdout_color_sinks.h"//在cpp中使用了stdout_color_mt，所以这里要包含这个头文件
 
 namespace Hazel {
     class HAZEL_API Log
@@ -32,3 +32,4 @@ namespace Hazel {
 #define HZ_WARN(...)          ::Hazel::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define HZ_ERROR(...)         ::Hazel::Log::GetClientLogger()->error(__VA_ARGS__)
 #define HZ_FATAL(...)         ::Hazel::Log::GetClientLogger()->critical(__VA_ARGS__)
+
